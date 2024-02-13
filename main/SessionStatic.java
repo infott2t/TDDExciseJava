341faac0e1cb55de330e305641a0f3407bc5c270
HYEONIL CHOI <tt2t2am1118@naver.com>
2024-02-13 13:21:04 +0900

    Update SessionStatic.java

--- main/SessionStatic.java
+++ main/SessionStatic.java
@@ -3,8 +3,12 @@ package main;
 public  class SessionStatic {
     
     static Customer LoginCustomer;
+    static TimeStamp0 Stamp0;
+    static String beforeResultStr="";
 
     SessionStatic(){
         LoginCustomer = null;
+        Stamp0 = null;
+        beforeResultStr = "";
     }
 }
