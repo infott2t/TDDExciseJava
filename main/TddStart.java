e77503260d612e6defe120a744c2137327a1be5b
HYEONIL CHOI <tt2t2am1118@naver.com>
2024-02-13 13:21:45 +0900

    Update TddStart.java

--- main/TddStart.java
+++ main/TddStart.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.Scanner;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class TddStart {
@@ -9,9 +10,9 @@ public class TddStart {
 
     public static void main(String args[]){
         System.out.print("\033[1;33m");
-        System.out.println("TDD~.");
+        System.out.println("TDD~. TDDExciseJava.");
         System.out.print("\033[0m");
-        System.out.println("Make man, @tt2t2am1118(twitter.com). Enjoy~.");
+        System.out.println("Enjoy~. This github, https://github.com/infott2t/TDDExciseJava\n");
         
         LoginCustomer = null; //로그인 초기화.
 
@@ -22,21 +23,33 @@ public class TddStart {
          
         //ID 초기화. - SQL 연결 필요
         ArrayList<Customer> customers = new ArrayList<>();
+        ArrayList<TimeStamp0> timeStamp0s = new ArrayList<>();
         
-        Customer cus = new Customer(1L, "Caisy","caisy@email.com","customer","1234");
+        Customer cus = new Customer(1L, "Caisy","caisy@email.com","Admin","1234");
+        //Role이 admin일 경우, 자격증 번호가 필요. 개인정보취급사 등. 
+        cus.setCertNum("00000000");
+
         customers.add(cus);
         do{
-            menuIndex = processMenu(menuIndex, menu, customers);
-            for(int i = 0; i < 5; i++) {
-                System.out.println();
+            menuIndex = processMenu(menuIndex, menu, customers, timeStamp0s);
+
+            try {
+                if (System.getProperty("os.name").contains("Windows")) {
+                    new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                } else {
+                    Runtime.getRuntime().exec("clear");
+                }
+            } catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
             }
+           
           
             menuIndex ++;
         }while(menuIndex >=0);
 
     }// main end
 
-    static int processMenu(int menuIndex, CustomMenu menu, ArrayList<Customer> customers){
+    static int processMenu(int menuIndex, CustomMenu menu, ArrayList<Customer> customers, ArrayList<TimeStamp0> timeStamp0s){
         if(SessionStatic.LoginCustomer!=null){
             if(SessionStatic.LoginCustomer.getId()!=null){
                 System.out.println("\033[1;32mStatus\033[0m: Log in, \033[1;34m"+SessionStatic.LoginCustomer.getIdName()+"\033[0m");
@@ -46,7 +59,7 @@ public class TddStart {
         }
        
         System.out.println("\033[1;32mString\033[0m [back] [break] [view menu]");
-        System.out.println("\033[1;32mMenu\033[0m \033[1;255m"+menuIndex+"\033[0m\033[1;32m *************************\033[0m");
+        System.out.println("\033[1;32mMenu\033[0m "+menuIndex+"\033[1;32m *************************\033[0m");
 
         menu.menuStrs(menuIndex);
         
@@ -57,12 +70,13 @@ public class TddStart {
             
             System.out.print("\033[1;34minput, : \033[0m");
             iStr = sc.nextLine();
-            System.out.println(iStr+ " input.");
+            //System.out.println(iStr+ " input.");
             
             if(iStr.equals("break")){
                 menuIndex = -2;
                 System.out.println("--------->Break.");
             }else if(iStr.equals("back")){
+                SessionStatic.beforeResultStr="";
                 if(menuIndex == 1){
                     menu.remove(menuIndex);
                     menuIndex = menuIndex-2;
@@ -79,17 +93,18 @@ public class TddStart {
              
             }else if(iStr.equals("view menu")){
                 
+
             }else if(iStr.equals("")){
                 System.out.println("No input..... .");
                 if(menuIndex == 0){
-
+                    menuIndex = menuIndex -1;
                 }else{
                     menuIndex = menuIndex -1;
                 }
-              
+                SessionStatic.beforeResultStr="";
                 
             }else{
-                    menuIndex = menu.runing(menuIndex, iStr, customers);
+                    menuIndex = menu.runing(menuIndex, iStr, customers,timeStamp0s);
                     
                 
             }
