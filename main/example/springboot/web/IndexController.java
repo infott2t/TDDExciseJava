4de6deb9727840220b5c85d6eaf97c417455af5f
HYEONIL CHOI <tt2t2am1118@naver.com>
2024-02-14 10:45:20 +0900

    Create IndexController.java

---
+++ main/example/springboot/web/IndexController.java
@@ -0,0 +1,26 @@
+package main.example.springboot.web;
+
+/* 책, p132쪽 참조. 
+ * folder: config, service, domain, web.  
+ * class, Controller 2 case. Controller or ApiController.
+ *
+ *  in there, web folder.
+ */
+
+/*
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+    private final PostsService postsService;
+
+    @GetMapping("/")
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
+        return "index";
+    }
+    ...
+*/
