ea9ce68380e45ddd037e7c93a9b252b542bc1a7b
HYEONIL CHOI <tt2t2am1118@naver.com>
2024-02-14 10:42:52 +0900

    Add files via upload

---
+++ main/example/springboot/domain/user/Role.java
@@ -0,0 +1,25 @@
+package main.example.springboot.domain.user;
+
+/* 책, p178쪽 참조. 
+ * folder: config, service, domain, web.  
+ * 
+ * enum, Role.java.
+ * 
+ * SQL data(Long, String, date, ...), and Role.
+ *
+ *  in there, domain.[entity_name] folder.
+ */
+
+/*
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+
+    GUEST("ROLE_GUEST", "guest-none-id"),
+    USER("ROLE_USER", "user");
+
+    private final String key;
+    private final String title;
+
+}
+*/
